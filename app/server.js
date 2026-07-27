a238f7faa4d36eb7086b1d255032ce3fe96c36f2
Jack McLean <jm4992@drexel.edu>
2026-07-27 12:13:33 -0400

    Creating server file

---
+++ app/server.js
@@ -0,0 +1,19 @@
+const pg = require("pg");
+const express = require("express");
+const app = express();
+
+const port = 3000;
+const hostname = "localhost";
+
+const env = require("../env.json");
+const Pool = pg.Pool;
+const pool = new Pool(env);
+pool.connect().then(function () {
+  console.log(`Connected to database ${env.database}`);
+});
+
+app.use(express.static("public"));
+
+app.listen(port, hostname, () => {
+  console.log(`Listening at: http://${hostname}:${port}`);
+});
