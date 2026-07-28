605f48fb229e9b17dabdfcb68e8eb3411f4e4e18
johnriley <jar632@drexel.edu>
2026-07-28 12:44:41 -0400

    Edited home and implemented odds API

--- app/server.js
+++ app/server.js
@@ -1,19 +1,105 @@
-const pg = require("pg");
-const express = require("express");
-const app = express();
+let express = require("express");
+let axios = require("axios");
+let path = require("path");
 
-const port = 3000;
-const hostname = "localhost";
+let app = express();
 
-const env = require("../env.json");
-const Pool = pg.Pool;
-const pool = new Pool(env);
-pool.connect().then(function () {
-  console.log(`Connected to database ${env.database}`);
+let hostname = "localhost";
+let port = 3000;
+
+let env = require("../env.json");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/games", async function (req, res) {
+    try {
+        let now = new Date();
+
+        let startOfToday = new Date(now);
+        startOfToday.setHours(0, 0, 0, 0);
+
+        let startOfTomorrow = new Date(startOfToday);
+        startOfTomorrow.setDate(startOfTomorrow.getDate() + 1);
+
+        let options = {
+            method: "GET",
+            url: env.api_url,
+            params: {
+                apiKey: env.api_key,
+                leagueID: "MLB",
+                startsAfter: startOfToday.toISOString(),
+                startsBefore: startOfTomorrow.toISOString(),
+                finalized: false,
+                oddsAvailable: true,
+                limit: 50
+            }
+        };
+
+        let response = await axios.request(options);
+
+        let events = response.data.data;
+
+        let games = events.map(function (event) {
+            let allOdds = Object.values(event.odds || {});
+
+            let awayMoneyline = allOdds.find(function (odd) {
+                return (
+                    odd.betTypeID === "ml" &&
+                    odd.periodID === "game" &&
+                    odd.sideID === "away"
+                );
+            });
+
+            let homeMoneyline = allOdds.find(function (odd) {
+                return (
+                    odd.betTypeID === "ml" &&
+                    odd.periodID === "game" &&
+                    odd.sideID === "home"
+                );
+            });
+
+            return {
+                eventID: event.eventID,
+                date: event.status.startsAt,
+
+                awayTeamName: event.teams.away.names.long,
+                homeTeamName: event.teams.home.names.long,
+
+                awayTeamID: event.teams.away.teamID,
+                homeTeamID: event.teams.home.teamID,
+
+                // if/else
+                awayOdds: awayMoneyline
+                    ? awayMoneyline.bookOdds
+                    : "N/A",
+
+                homeOdds: homeMoneyline
+                    ? homeMoneyline.bookOdds
+                    : "N/A"
+            };
+        });
+
+        console.log("Games being sent to browser:");
+        console.log(games);
+
+        res.json(games);
+    }
+    catch (error) {
+        console.error(
+            error.response
+                ? error.response.data
+                : error.message
+        );
+
+        res.status(500).json({
+            success: false,
+            error: error.response
+                ? error.response.data
+                : error.message
+        });
+    }
 });
 
-app.use(express.static("public"));
-
-app.listen(port, hostname, () => {
-  console.log(`Listening at: http://${hostname}:${port}`);
+app.listen(port, hostname, function () {
+    console.log(`Listening at http://${hostname}:${port}`);
 });
