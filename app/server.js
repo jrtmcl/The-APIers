e8d92593fbdae4d12002dce1d57a47e48ef627c5
DevikaM <devika7097@gmail.com>
2026-08-17 18:13:35 -0400

    Added login page

--- app/server.js
+++ app/server.js
@@ -267,6 +267,11 @@ app.get("/how-to", function (req, res) {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
+app.get("/login", function (req, res) {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+
 app.get("/my-bets", function (req, res) {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
